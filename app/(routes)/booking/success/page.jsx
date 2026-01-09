db8e0f8e7b2f7f9299ff392e8f85b87f536dbcbe
Krishn <22amtics070@gmail.com>
2026-01-09 10:19:41 +0530

    Updated Success Page

--- app/(routes)/booking/success/page.jsx
+++ app/(routes)/booking/success/page.jsx
@@ -1,5 +1,6 @@
 "use client";
 
+export const dynamic = "force-dynamic";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { useSearchParams } from "next/navigation";
